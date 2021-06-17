e6bc901272994f9228f9d0c4bc226115a943c611
CVHuber <72010077+CVHuber@users.noreply.github.com>
2021-06-17 15:30:36 +0800

    Create  Res2Net Convotion.py

---
+++ Res2Net Convotion.py
@@ -0,0 +1,96 @@
+import torch.nn as nn
+import math
+import torch.utils.model_zoo as model_zoo
+import torch
+import torch.nn.functional as F
+__all__ = ['Res2Net', 'res2net50']
+
+# source: https://github.com/Res2Net/Res2Net-PretrainedModels/blob/master/res2net.py
+
+model_urls = {
+    'res2net50_26w_4s': 'https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net50_26w_4s-06e79181.pth',
+    'res2net50_48w_2s': 'https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net50_48w_2s-afed724a.pth',
+    'res2net50_14w_8s': 'https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net50_14w_8s-6527dddc.pth',
+    'res2net50_26w_6s': 'https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net50_26w_6s-19041792.pth',
+    'res2net50_26w_8s': 'https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net50_26w_8s-2c7c9f12.pth',
+    'res2net101_26w_4s': 'https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net101_26w_4s-02a759a1.pth',
+}
+
+
+class Bottle2neck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, baseWidth=26, scale = 4, stype='normal'):
+        """ Constructor
+        Args:
+            inplanes: input channel dimensionality
+            planes: output channel dimensionality
+            stride: conv stride. Replaces pooling layer.
+            downsample: None when stride = 1
+            baseWidth: basic width of conv3x3
+            scale: number of scale.
+            type: 'normal': normal set. 'stage': first block of a new stage.
+        """
+        super(Bottle2neck, self).__init__()
+
+        width = int(math.floor(planes * (baseWidth/64.0)))
+        self.conv1 = nn.Conv2d(inplanes, width*scale, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(width*scale)
+        
+        if scale == 1:
+          self.nums = 1
+        else:
+          self.nums = scale -1
+        if stype == 'stage':
+            self.pool = nn.AvgPool2d(kernel_size=3, stride = stride, padding=1)
+        convs = []
+        bns = []
+        for i in range(self.nums):
+          convs.append(nn.Conv2d(width, width, kernel_size=3, stride = stride, padding=1, bias=False))
+          bns.append(nn.BatchNorm2d(width))
+        self.convs = nn.ModuleList(convs)
+        self.bns = nn.ModuleList(bns)
+
+        self.conv3 = nn.Conv2d(width*scale, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stype = stype
+        self.scale = scale
+        self.width  = width
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        spx = torch.split(out, self.width, 1)
+        for i in range(self.nums):
+          if i==0 or self.stype=='stage':
+            sp = spx[i]
+          else:
+            sp = sp + spx[i]
+          sp = self.convs[i](sp)
+          sp = self.relu(self.bns[i](sp))
+          if i==0:
+            out = sp
+          else:
+            out = torch.cat((out, sp), 1)
+        if self.scale != 1 and self.stype=='normal':
+          out = torch.cat((out, spx[self.nums]),1)
+        elif self.scale != 1 and self.stype=='stage':
+          out = torch.cat((out, self.pool(spx[self.nums])),1)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
